cbeb2ac56fcfd7aa48a33b2cf769c8604134a915
Sincere-Dhairya <varshneydhairya4@gmail.com>
2022-03-12 15:31:20 +0530

    deploy commit 3

--- Guide/settings.py
+++ Guide/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['pyfindv.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['pyfindv.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
